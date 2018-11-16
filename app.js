a91c17869834bc87ab85b730a4a825f591c351b3
bill1812 <bill1812@users.noreply.github.com>
2018-11-16 19:27:00 +0800

    ES6 supported ... TEST

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
-var createError = require('http-errors');
-var mimeTIE = 'text/html';
-var express = require('express');
-var path = require('path');
-var compression = require('compression');
-var cookieParser = require('cookie-parser');
-var favicon = require('serve-favicon');
-var frameguard = require('frameguard');
-var logger = require('morgan');
-var mimetype = require('mime-types');
-var sassMiddleware = require('node-sass-middleware');
+// app.modules
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const favicon = require('serve-favicon');
+const frameguard = require('frameguard');
+const logger = require('morgan');
+const mimetype = require('mime-types');
+const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
+const xfo = require('./bin/www');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// app.localModules
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
-app.disable('x-powered-by');
+// app.localVariable
+const app = express();
 
-// remove following line remark cuz compression() work before view engine
+// app.localLetVar
+let mimeTIE = 'text/html';
+// app.disable('x-powered-by');
+
+// app.uses
 app.use(compression());
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev' /* options: immediate, skip, stream, combined, common, [dev], short, tiny */ ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  const cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    let randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    res.cookie(
+      'yummyCookie',
+      randomNumber,
+      { maxAge: 0.5 * 60 * 60,
+        httpOnly: true,
+        signed: false,
+        sameSite: true
+      }
+    );
+    console.log(`\x1b[35m\x1b[1m`, ` cookie: ` + randomNumber, `\x1b[37m\x1b[0m`);
+  }
+  next();
+});
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(frameguard());
+app.use(frameguard({
+  action: xfo.xframeorigin //,
+  //domain: '',
+  //domain: '',
+  //domain: '',
+  //domain: ''
+}));
 app.use(sassMiddleware({
    src: path.join(__dirname, 'sass'),
   dest: path.join(__dirname, 'public/css'),
@@ -36,7 +61,7 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss 
   sourceMap: false,
   prefix: '/css',
-  outputStyle: 'expanded' // 'nested' | 'expanded' | 'compact' | 'compressed' 
+  outputStyle: 'expanded' // 'nested' | [expanded] | 'compact' | 'compressed' 
 }));
 app.use(express.static(path.join(__dirname, 'public'),
   { dotfiles: 'ignore',
@@ -48,35 +73,54 @@ app.use(express.static(path.join(__dirname, 'public'),
     lastModified: true,
     maxAge: '1h',
     redirect: true,
-    setHeaders: function (res, path, stat) {
+    setHeaders: (res, path, stat) => {
       res.set('x-timestamp', Date.now());
       mimeTIE = mimetype.lookup(path);
       if (mimeTIE !== 'text/html') {
         res.setHeader('Cache-Control', 'public, max-age=1800');
-        console.log('path: ' + path);
+        res.setHeader('Expires', new Date(Date.now() + 2 * 60* 60).toUTCString());
+    //  console.log('path: ' + path);
       }
-      console.log('Current mimeTypes: ' + mimeTIE);
+    //  console.log('Current mimeTypes: ' + mimeTIE);
     }
   }
 ));
 
+// app.mounts
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// view engine setup @app.view.render
+
+// app.view.engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+
+  const theErr = [
+    req.app.get('env'),
+    res.statusCode.toString()
+  ];
+  // console.log('theErr[0]: ' + theErr[0] + ' theErr[1]: ' + theErr[1]);
+  if (theErr[0] === 'development') {
+    res.render('error');
+  } else {
+    res.render(theErr[1]);
+  }
+  next();
 });
 
 module.exports = app;
